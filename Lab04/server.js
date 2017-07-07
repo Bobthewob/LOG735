e4cb69abeb5700e1e370679db013c21072a48b9c
Bobthewob <phillippe118@gmail.com>
2017-07-07 17:27:50 -0400

    Début exclusion mutelle

--- Lab04/server.js
+++ Lab04/server.js
@@ -4,34 +4,112 @@ const CryptoJS = require("crypto-js");
 const PrivateKey = 'LOG735';
 const ServerPort = process.argv[2];
 
-var ws = new WebSocketServer( { port: ServerPort } );
+var wss = new WebSocketServer( { port: ServerPort } );
 console.log("Server started...");
 
-//id sent to workspaces;
-var currentId = 1;
-
+var currentId = 0;
+var workspaces = [];
+var sharedText = '';
+var currentWriter = null;
+var writingFifo = [];
 
 //Called when a workspace connects to the server
-ws.on('connection', function (ws) {
+wss.on('connection', function (ws) {
   	console.log("Browser connected online...");
-   
+  	//A new workspace connects and requests and ID from the server
+  	ws.id = currentId;
+  	workspaces[ws.id] = ws;
+  	ws.send('{ "type":"idRequest", "id":"'+ crypt(currentId.toString()) +' "}'); //Informs the workspace of his ID
+  	currentId++;
+
+    //When a workspace sends a message to the server
   	ws.on("message", function (str) {
 	    var object = JSON.parse(str);
+
 	    switch(object.type) {
-	        //A new workspace connects and requests and ID from the server
-	        case 'idRequest':
-		        console.log("Received idRequest");     
-    			ws.send('{ "type":"idRequest", "id":"'+ crypt(currentId.toString()) +' "}'); 
-		        currentId++;
+	    	//Sets the nickname for the workspace
+		    case 'nicknameRequest':
+				workspaces[ws.id].nickname = decrypt(object.nickname); //Saves nickname
+		        var data = '{ "type":"newUser", "nickname":"'+ object.nickname +' "}';
+		        broadcastToEveryoneElse(ws, data);
 		        break;
-	    	}   
+
+		    //
+		    case 'writingRequest':
+				writingRequest(ws.id);
+
+				if (currentWriter == ws.id) {
+					ws.send('{ "type":"hasRights" }');
+		        	//var data = '{ "type":"newWriter", "nickname":"'+ crypt(ws.nickname) +' "}';
+					//broadcastToEveryoneElse(data);
+				}
+		        break;
+
+		    case 'releaseRequest':				
+		        break;
+	    }   
     })
 
+  	//When a client is closed, inform other workspaces
     ws.on("close", function() {
-        console.log("Browser gone.")
+    	var data = '{ "type":"userLeft", "nickname":"'+ crypt(ws.nickname) +' "}';
+    	broadcastToEveryoneElse(ws, data);
+    	workspaces.splice(ws.id); //removes the closed workspace from the list
+    	removeFromFifo(ws.id); //removes from fifo
     })
 });
 
+
+//broadcasts a message to everyone except the sender
+function broadcastToEveryoneElse(ws, data) {
+	//Broadcast to other workspaces
+	wss.clients.forEach(function each(client) {
+	  if (client !== ws && client.readyState === 1) {
+	    client.send(data);
+	  }
+	});
+}
+
+//broadcasts a message to everyoneS
+function broadcast(data) {
+	//Broadcast to other workspaces
+	wss.clients.forEach(function each(client) {
+	  if (client.readyState === 1) {
+	    client.send(data);
+	  }
+	});
+}
+
+//Manage insertion into Fifo and sets currentWriter if empty
+function writingRequest(workspaceId) {
+	//Make sure the workspace is not already in the FIFO
+	if (writingFifo.indexOf(workspaceId) == -1) {
+		//Check is FIFO is empty, if so sets current writer
+		if (writingFifo.length == 0 && currentWriter == null) {
+			currentWriter = workspaceId;
+		}
+		else {
+			writingFifo.push(workspaceId);
+		}
+	}
+}
+
+//Used a workspace is closed ans it was still in the fifo
+function removeFromFifo(workspaceId) {
+	var index = writingFifo.indexOf(workspaceId);
+
+	if (index != -1) {
+	    writingFifo.splice(index, 1);
+	}
+}
+
+//Current writer releases the his writing rights
+function shiftFifo() {
+	if (writingFifo.length > 0) {
+		currentWriter = writingFifo.shift();
+		return currentWriter;
+	}
+}
 
 //Returns a crypted object
 function crypt(object) {
--- Lab04/workspace.js
+++ Lab04/workspace.js
@@ -5,7 +5,7 @@ var id;
 var nickname;
 
 //Login button click method. Sets nickname and calls connectToServer with input values
-$( "#login" ).click(function() {
+$( "#btnLogin" ).click(function() {
   if($.trim($('#txtNickname').val()) == '' || $.trim($('#txtIpAddress').val()) == '' || $.trim($('#txtPort').val()) == '') {
       alert('You must enter a nickname, an IP address and a port');
   }
@@ -13,44 +13,74 @@ $( "#login" ).click(function() {
     nickname = $.trim($('#txtNickname').val());
     var ipAddress = $.trim($('#txtIpAddress').val());
     var port = $.trim($('#txtPort').val());
-
-    var state = connectToServer(ipAddress, port, true);
+    connectToServer(ipAddress, port, true);
+    $('#btnLogin').prop('disabled', true);
   }  
 });
 
-
 //Tries to establish a connection with the ipAddress and port passed in parameters
 function connectToServer(ipAddress, port, firstConnection) {
-  ws = new WebSocket("ws://"+ipAddress+":"+port);
+	ws = new WebSocket("ws://"+ipAddress+":"+port);
 
-  ws.onopen = function (event) {
-    ws.send('{ "type":"idRequest" }'); 
+	ws.onopen = function (event) {
+		if (firstConnection) {
+		  $("#msgLoginSuccess").css("display", "").delay(5000).fadeOut(400);
+		}
+	};
 
-    if (firstConnection) {
-      $("#msgLoginSuccess").css("display", "").delay(5000).fadeOut(400);
-    }
-  };
+	ws.onmessage = function(event) { 
+		var message = JSON.parse(event.data);
 
-  ws.onmessage = function(event) { 
-    var message = JSON.parse(event.data);
-    console.log(message);
-    switch(message.type) {
-      case "idRequest":
-        console.log(decrypt(message.id));
-        break;
-    }
-  };
+		switch(message.type) {
+		  case "idRequest":
+		    id = message.id;
+		    ws.send('{ "type":"nicknameRequest", "nickname":"' + crypt(nickname)+ '" }');
+		    break;
 
-  ws.onerror = function(event){
-    if (firstConnection) {
-      $("#msgLoginFailure").css("display", "").delay(5000).fadeOut(400);
-    }
-  };
+			case "newUser":
+			    nickname = decrypt(message.nickname);
+			    logInfo(nickname + " has joined the session");
+			    break;
 
-  ws.onclose = function (event) {
-    console.log("serveur fermé");
-  };
+			case "userLeft":
+			    nickname = decrypt(message.nickname);
+			    logInfo(nickname + " has left the session");
+			    break;
 
+			case "hasRights":
+			    document.getElementById("sharedText").readOnly = false;
+			    $("#lblCurrentWriter").text("Current writer : " + nickname);
+			    logInfo("You know have writing rights!");
+			    break;
+
+			case "newWriter":
+			    nickname = decrypt(message.nickname);
+			    logInfo(nickname + " now has writing rights!");
+			    $("#lblCurrentWriter").text("Current writer : " + nickname);
+			    break;
+		}
+	};
+
+	ws.onerror = function(event){
+		if (firstConnection) {
+		  $("#msgLoginFailure").css("display", "").delay(5000).fadeOut(400);
+		}
+	};
+
+	ws.onclose = function (event) {
+		console.log("serveur fermé");
+	};
+
+	//Requests access to write
+	$( "#btnRequestRight").click(function() {
+		ws.send('{ "type":"writingRequest" }');
+	});
+
+}
+
+//Logs the info passed by parameter
+function logInfo(info) {
+	document.getElementById("txtLog").value += info + "\n";
 }
 
 //Returns a crypted object
