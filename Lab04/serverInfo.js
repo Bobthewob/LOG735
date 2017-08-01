04dfca28060adaf113654061152af1844c7beed0
capitainBoivin <catherineboivin.cb@gmail.com>
2017-08-01 14:11:59 -0400

    Fix de la faute d'ortograf -0.5 pts LOL TROLOLOL

--- Lab04/serverInfo.js
+++ Lab04/serverInfo.js
@@ -9,9 +9,9 @@ var ServerInfo = {};
 //Simply initialize the array with other servers info
 ServerInfo.initializeServerList = function(object) {
 	var serverList = {};
-	serverList["server1"] = {"serverName":"server1", "ip":"127.0.0.1", "clientPort":"8080", "serverPort":"8081"};
-	serverList["server2"] = {"serverName":"server2", "ip":"127.0.0.1", "clientPort":"8082", "serverPort":"8083"};
-	serverList["server3"] = {"serverName":"server3", "ip":"127.0.0.1", "clientPort":"8084", "serverPort":"8085"};
+	serverList["server1"] = {"serverName":"server1", "ip":"10.196.115.27", "clientPort":"8080", "serverPort":"8081"};
+	serverList["server2"] = {"serverName":"server2", "ip":"10.196.115.27", "clientPort":"8082", "serverPort":"8083"};
+	serverList["server3"] = {"serverName":"server3", "ip":"10.196.115.24", "clientPort":"8084", "serverPort":"8085"};
 	return serverList;
 }
 
